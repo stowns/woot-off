6730413560f155eafb38b40014e6842c643ed858
stowns <stowns3@gmail.com>
2021-03-30 09:58:39 -0500

    fix link crawling which is now relative

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ let interval = setInterval(async () => {
             lastItem = currentItem;
             // dig deeper and get the specific item details
             let href = $(".main-title").parent().attr('href');
-            let detailHtml = await rp(href);
+            let detailHtml = await rp(`https://sellout.woot.com${href}`);
+            console.log(detailHtml)
             $ = cheerio.load(detailHtml);
             let price = $('.price-exact > .price').text();
             let discount = $('.price-exact > .discount > .percentage').text();
